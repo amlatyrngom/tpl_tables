4b2c6086b790f758638dc435777a7741fcd9001b
Amadou Latyr Ngom <amlatyr@gmail.com>
2020-07-03 09:12:34 -0400

    Reduce space usage

--- gen_linetable.sh
+++ gen_linetable.sh
@@ -1,8 +1,9 @@
 # Create tables
 cd tpch-dbgen
 make
-./dbgen -T L -f -s $1
+./dbgen -T o -f -s $1
 mv lineitem.tbl ../tables
+mv orders.tbl ../tables
 cd ../tables
 
 echo "Formatting..."
@@ -11,7 +12,7 @@ echo "Formatting..."
 #sed -i 's/[|]$//' supplier.tbl
 #sed -i 's/[|]$//' partsupp.tbl
 #sed -i 's/[|]$//' customer.tbl
-#sed -i 's/[|]$//' orders.tbl
+sed -i 's/[|]$//' orders.tbl
 sed -i 's/[|]$//' lineitem.tbl
 #sed -i 's/[|]$//' nation.tbl
 #sed -i 's/[|]$//' region.tbl
@@ -22,19 +23,20 @@ echo "Done format"
 #python gen_tpch.py supplier
 #python gen_tpch.py partsupp
 #python gen_tpch.py customer
-#python gen_tpch.py orders
+python gen_tpch.py orders
 python gen_tpch.py lineitem
 #python gen_tpch.py nation
 #python gen_tpch.py region
 
 rm lineitem.tbl
+rm orders.tbl
 
 # Rename to .data
 #mv part.data part.tbl
 #mv supplier.data supplier.tbl
 #mv customer.data customer.tbl
 #mv partsupp.data partsupp.tbl
-#mv orders.data orders.tbl
+mv orders.data orders.tbl
 mv lineitem.data lineitem.tbl
 #mv nation.data nation.tbl
 #mv region.data region.tbl
--- tables/gen_tpch.py
+++ tables/gen_tpch.py
@@ -4,32 +4,67 @@ import sys
 
 def DictCompress(col):
     unique_keys = col.unique()
-    sorted(unique_keys)
     compress_dict = {}
     for (i, val) in enumerate(unique_keys):
         compress_dict[val] = i
     return compress_dict
+
+#
+def AddDictCompress(col, compress_dict):
+    unique_keys = col.unique()
+    i = len(compress_dict)
+    for val in unique_keys:
+        if val not in compress_dict:
+            compress_dict[val] = i
+            i += 1
 
 
 def gen_lineitem(table_name):
     print("Cleaning up csv file")
     tbl_file = table_name + ".tbl"
     data_file = table_name + ".data"
-    df = pd.read_csv(tbl_file, sep='|', header=None, index_col=False)
-    returnflag = df[8]
-    returnflag_dict = DictCompress(returnflag)
-    linestatus = df[9]
-    linestatus_dict = DictCompress(linestatus)
-    df[8] = returnflag.map(lambda x: returnflag_dict[x])
-    df[9] = linestatus.map(lambda x: linestatus_dict[x])
+    dfs = pd.read_csv(tbl_file, sep='|', header=None, index_col=False, chunksize=5000000, usecols=[0, 4, 5, 6, 7, 8, 9, 10])
+    first = True
+    returnflag_dict = {}
+    linestatus_dict = {}
+    max_idx = 0
+    for df in dfs:
+        print(df.head())
+        returnflag = df[8]
+        AddDictCompress(returnflag, returnflag_dict)
+        linestatus = df[9]
+        AddDictCompress(linestatus, linestatus_dict)
+        print(returnflag_dict)
+        print(linestatus_dict)
+        df[8] = returnflag.map(lambda x: returnflag_dict[x])
+        df[9] = linestatus.map(lambda x: linestatus_dict[x])
+        if (first):
+            first = False
+            df.to_csv(data_file, sep='|', index=False)
+        df.to_csv(data_file, sep='|', index=False, mode='a', header=False)
+    print(returnflag_dict)
+    print(linestatus_dict)
+    print("Cleaned up csv file")
+
+def gen_orders(table_name):
+    print("Cleaning up csv file")
+    tbl_file = table_name + ".tbl"
+    data_file = table_name + ".data"
+    df = pd.read_csv(tbl_file, sep='|', header=None, index_col=False, usecols=[0, 4, 5])
+    orderpriority = df[5]
+    orderpriority_dict = DictCompress(orderpriority)
+    df[5] = orderpriority.map(lambda x : orderpriority_dict[x])
     df.to_csv(data_file, sep='|', index=False)
     print("Cleaned up csv file")
+
 
 
 #### Generate CSV file ####
 def gen_csv(table_name):
     if (table_name == "lineitem"):
         return gen_lineitem(table_name)
+    if (table_name == "orders"):
+        return gen_orders(table_name)
     print("Cleaning up csv file")
     tbl_file = table_name + ".tbl"
     data_file = table_name + ".data"
